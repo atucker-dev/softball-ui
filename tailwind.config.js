36b73ebfd89fa93d639807ea4d7760e633bfe742
JMorgCode <jmorgan1523@gmail.com>
2024-01-17 15:19:55 -0700

    basic login pahge

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue}'], // paths to all of your files
+  theme: {
+    extend: {
+      // Extend the default Tailwind styles here
+      colors: {
+        // Define custom colors here
+      },
+      spacing: {
+        // Define custom spacing here
+      },
+      // ... other theme extensions
+    },
+  },
+  plugins: [
+    // Add Tailwind plugins here
+  ],
+  // Other configurations...
+};
